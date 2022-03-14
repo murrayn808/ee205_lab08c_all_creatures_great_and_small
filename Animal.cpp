5c5c2b869e69d4de8d0f236f805cc5e0cc32581e
murrayn808 <murrayn@hawaii.edu>
2022-03-13 20:17:24 -1000

    initial commit. Add member variables

--- Animal.cpp
+++ Animal.cpp
@@ -2,16 +2,14 @@
 ///         University of Hawaii, College of Engineering
 /// @brief  ee205_lab08c_all_creatures_great_and_small - EE 205 - Spr 2022
 ///
-/// @file main.cpp
+/// @file Animal.cpp
 /// @version 1.0
 ///
 /// @author <murrayn@hawaii.edu>
 /// @date   13_Mar_2022
 ///////////////////////////////////////////////////////////////////////////////
+#include "Animal.h"
 
-#include <iostream>
+using namespace std;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
+const string Animal::kingdom = "Animalia";
